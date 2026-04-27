eb94371d1e15a264198568f1a316026350d0a248
Jarkko Remes <ag1534@student.jamk.fi>
2026-04-27 13:59:31 +0300

    Added fake products for testing purposes

--- frontend/src/app/product.ts
+++ frontend/src/app/product.ts
@@ -3,7 +3,51 @@ export interface Iproduct {
   productName: string;
   brand: string;
   category: string;
-  expirationDate: string;
+  expirationDate: number; //time in milliseconds
   openedDate: string;
   s3ImageKey: string;
+  isDeleted: boolean;
 }
+
+export const mockProducts: Iproduct[] = [
+  {
+    ItemId: '1',
+    productName: 'Rasvaton Maito',
+    brand: 'Valio',
+    category: 'Maitotuotteet',
+    expirationDate: 1576800000000,
+    openedDate: '2026-04-27',
+    s3ImageKey: 'randomtext',
+    isDeleted: false,
+  },
+  {
+    ItemId: '2',
+    productName: 'Jogurtti',
+    brand: 'Arla',
+    category: 'Maitotuotteet',
+    expirationDate: 1577800000000,
+    openedDate: '2026-05-01',
+    s3ImageKey: 'anotherrandom',
+    isDeleted: false,
+  },
+  {
+    ItemId: '3',
+    productName: 'Leipä',
+    brand: 'Fazer',
+    category: 'Leivät',
+    expirationDate: 1578800000000,
+    openedDate: '2026-05-10',
+    s3ImageKey: 'moretext',
+    isDeleted: false,
+  },
+  {
+    ItemId: '4',
+    productName: 'Omena',
+    brand: 'Luomu',
+    category: 'Hedelmät',
+    expirationDate: 1579800000000,
+    openedDate: '2026-05-20',
+    s3ImageKey: 'keyhere',
+    isDeleted: false,
+  },
+];
