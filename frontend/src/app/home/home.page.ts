87715127974e3e235632fb1fa96a794211f59b54
Jarkko Remes <ag1534@student.jamk.fi>
2026-04-27 16:19:33 +0300

    Added product list to home page, added product-card component

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {
@@ -8,7 +8,8 @@ import {
   IonToolbar,
 } from '@ionic/angular/standalone';
 import { SummaryCardComponent } from '../summary-card/summary-card.component';
-
+import { Iproduct, mockProducts } from '../product';
+import { ProductCardComponent } from '../product-card/product-card.component';
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -22,9 +23,11 @@ import { SummaryCardComponent } from '../summary-card/summary-card.component';
     CommonModule,
     FormsModule,
     SummaryCardComponent,
+    ProductCardComponent,
   ],
 })
 export class HomePage implements OnInit {
+  productList = signal<Iproduct[]>([...mockProducts]);
   number1 = 1; // number for testing
   constructor() {}
 
--- frontend/src/app/product-card/product-card.component.ts
+++ frontend/src/app/product-card/product-card.component.ts
@@ -1,14 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, input, OnInit, signal } from '@angular/core';
+import { Iproduct } from '../product';
+import {
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonCardSubtitle,
+  IonChip,
+  IonIcon,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss'],
+  imports: [
+    IonIcon,
+    IonChip,
+    IonCardSubtitle,
+    IonCardContent,
+    IonCardTitle,
+    IonCard,
+    IonCardHeader,
+  ],
 })
-export class ProductCardComponent  implements OnInit {
+export class ProductCardComponent implements OnInit {
+  product = input<Iproduct>();
+  lifeLeft = signal<number>(0); // How many days until expires
+  expirationDate = signal<string>('');
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  getDaysLeft(ISODate: string) {
+    const nonISODate = Date.parse(ISODate); //conver ISOdate to milliseconds
+    const dateDiff = nonISODate - Date.now(); //calculate millisecond difference between 2 dates
+    const dayDiff = Math.ceil(dateDiff / (1000 * 3600 * 24)); //Conver milliseconds to days
+    return dayDiff;
+  }
+  ngOnInit() {
+    this.lifeLeft.set(this.getDaysLeft(this.product()!.expirationDate));
+    this.expirationDate.set(
+      `${new Date(this.product()!.expirationDate).getDate()}.${new Date(this.product()!.expirationDate).getMonth()}.`,
+    ); // Set date to dd.mm.
+  }
+  // Sets the chip color to green, yellow or red
+  getColor() {
+    if (this.lifeLeft() > 3) {
+      return 'success';
+    } else if (this.lifeLeft() >= 0) {
+      return 'warning';
+    } else {
+      return 'danger';
+    }
+  }
+  // Sets the chip icon based on lifeLeft
+  getIcon() {
+    if (this.lifeLeft() > 3) {
+      return 'checkmark-circle-outline';
+    } else if (this.lifeLeft() >= 0) {
+      return 'time-outline';
+    } else {
+      return 'alert-circle-outline';
+    }
+  }
 }
