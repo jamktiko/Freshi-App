d7d57148adf68ea0ac565884cc15bb4fa23a03fa
Konsta Poikolainen <ag1539@student.jamk.fi>
2026-04-29 13:39:17 +0300

    fixed issue with middleware import on routes/userDevices.routes.js

--- backend/routes/userDevices.routes.js
+++ backend/routes/userDevices.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { requireAuth } from '../middleware/authMiddleware.js'; // Uncomment this line to enable authentication for item routes
+import { requireAuth } from '../middleware/auth.middleware.js'; // Uncomment this line to enable authentication for item routes
 
 import {
 getDevicesForUser, 
