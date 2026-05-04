36cdb98c864c040711e521169e3c7e49928f56b2
Jarkko Remes <ag1534@student.jamk.fi>
2026-05-05 01:17:50 +0300

    Disabled guards temporarily, due to slow activation and preventing going to home page automatically

--- frontend/src/app/tabs/tabs.routes.ts
+++ frontend/src/app/tabs/tabs.routes.ts
@@ -12,37 +12,31 @@ export const routes: Routes = [
         path: 'register',
         loadComponent: () =>
           import('../register/register.page').then((m) => m.RegisterPage),
-        canActivate: [loggedInGuard],
       },
       {
         path: 'confirm',
         loadComponent: () =>
           import('../confirm/confirm.page').then((m) => m.ConfirmPage),
-        canActivate: [loggedInGuard],
       },
       {
         path: 'login',
         loadComponent: () =>
           import('../login/login.page').then((m) => m.LoginPage),
-        canActivate: [loggedInGuard],
       },
       {
         path: 'home',
         loadComponent: () =>
           import('../home/home.page').then((m) => m.HomePage),
-        canActivate: [authGuard],
       },
       {
         path: 'welcome',
         loadComponent: () =>
           import('../welcome/welcome.page').then((m) => m.WelcomePage),
-        canActivate: [loggedInGuard],
       },
       {
         path: 'settings',
         loadComponent: () =>
           import('../settings/settings.page').then((m) => m.SettingsPage),
-        canActivate: [authGuard],
       },
       {
         path: '',
